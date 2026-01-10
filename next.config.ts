0a5a3bf42dabe5966b6a51a3e205a56a06cd17ef
Koro03 <benjamin.fauchet0309@gmail.com>
2026-01-10 21:15:00 +0100

    Preparation for deployement

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   output: "export",
   images: {
     unoptimized: true,
